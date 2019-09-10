6c50ddaf1c59e70ae7bea2a6756ec37b8e39e1c5
NIlsSchnitzel <235770@student.insulacollege.nl>
2019-09-10 12:26:48 +0200

    Java_Script.1

---
+++ b1w2l1.js
@@ -0,0 +1,10 @@
+alert(' Hallo Rompie');  
+document.write('Voornaam en achternaam')
+document.write(prompt('Vul je naam in')); // Zegt je naam
+document.write('<br/>')
+document.write('Leeftijd')
+document.write(prompt('Vul je leeftijd in'));
+document.write('<br/>')
+document.write('Opleiding')
+document.write(prompt('Naam opleiding'));
+document.write('<br/>')
